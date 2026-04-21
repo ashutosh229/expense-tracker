6f11b4f5af315f2c1d565a63cacfde90fcd4d164
ashutosh229 <akumarjha875@gmail.com>
2026-04-22 01:31:20 +0530

    feat: implement the button for authentication module

---
+++ lib/src/features/auth/presentation/widgets/primary_auth_button.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class PrimaryAuthButton extends StatelessWidget {
+  const PrimaryAuthButton({
+    required this.label,
+    required this.onPressed,
+    super.key,
+  });
+
+  final String label;
+  final VoidCallback onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: onPressed,
+      child: Text(label),
+    );
+  }
+}
